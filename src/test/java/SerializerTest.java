98c0c02b032ee184c3c37b214b2127f2cab039ee
Noctoid <i@noctoid.com>
2020-09-23 16:17:47 +0800

    rewrite tests to junit

--- src/test/java/SerializerTest.java
+++ src/test/java/SerializerTest.java
@@ -4,67 +4,80 @@ import dmpUtil.DMPUtil;
 import serializer.Serializer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Supplier;
 
-public class Test {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Testing Serializer Methods");
-        System.out.println("===========================================");
+public class SerializerTest {
 
-        System.out.println("Constructor");
-        System.out.println("===========================================");
+    @Test
+    void ConstructorTest() {
+        Serializer serializer = new Serializer();
+        assertNotEquals(serializer, null);
+    }
+
+    @Test
+    void LoadFilesTest() {
+        Serializer serializer = new Serializer();
+        serializer.loadRenameMappingFromFile(new File("src/test/resources/rename_mapping_1.txt"));
+    }
+
+    @Test
+    void FlattenTest() throws IOException {
         Serializer serializer = new Serializer();
 
-        System.out.println("LoadRenameFile");
-        System.out.println("===========================================");
-        serializer.loadRenameMappingFromFile(new File("src/test/resources/rename_mapping_1.txt"));
-
-        System.out.println("loadJsonFromFile");
-        System.out.println("===========================================");
         String[] testJsonFiles = new String[]{
-                "src/test/resources/test-1.json",
-                "src/test/resources/test-2.json"
+            "src/test/resources/test-1.json",
+            "src/test/resources/test-2.json"
         };
         ArrayList<JSONObject> testJsons = new ArrayList<>();
         for (String testJsonFile : testJsonFiles) {
             JSONObject object = serializer.loadJsonFromFile(new File(testJsonFile));
             testJsons.add(object);
         }
-
-        System.out.println("serialize");
-        System.out.println("===========================================");
-
         for (JSONObject object: testJsons) {
             String serializedObject = serializer.flatten(object);
             System.out.println(serializedObject);
         }
+    }
 
-        System.out.println("deserialize");
-        System.out.println("===========================================");
-
+    @Test
+    void ReformTest() {
+        Serializer serializer = new Serializer();
         String[] testTxtFiles = new String[] {
-                "src/test/resources/test-0.txt",
-                "src/test/resources/test-1.txt"
-        };
+            "src/test/resources/test-0.txt",
+            "src/test/resources/test-1.txt"
+    };
 
         for (String txtFile: testTxtFiles) {
-            File rawFile = new File(txtFile);
-            String rawFileContent = new Scanner(rawFile).useDelimiter("\\Z").next();
-            JSONObject object = serializer.reform(rawFileContent);
-            System.out.println(JSONObject.toJSONString(object, SerializerFeature.WriteMapNullValue));
-        }
+            try {
+                File rawFile = new File(txtFile);
+                Scanner scanner = new Scanner(rawFile).useDelimiter("\\Z");
+                String rawFileContent = scanner.next();
+                JSONObject object = serializer.reform(rawFileContent);
+                System.out.println(JSONObject.toJSONString(object, SerializerFeature.WriteMapNullValue));
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+    }
 
-        System.out.println("DMPUtil.GenerateDMPInput");
-        System.out.println("===========================================");
-
+    @Test
+    void DmpInputTest() {
+        Serializer serializer = new Serializer();
         String[] baseLineFiles = new String[] {
-                "src/test/resources/DMP/baseline1.json;src/test/resources/DMP/modification1.txt",
+            "src/test/resources/DMP/baseline1.json;src/test/resources/DMP/modification1.txt",
         };
 
         for (String filePair: baseLineFiles) {
+            try {
             String[] baselineAndModification = filePair.split(";");
             String baselineFile = baselineAndModification[0];
             String modificationFile = baselineAndModification[1];
@@ -76,7 +89,11 @@ public class Test {
             DMPUtil dmpUtil = new DMPUtil();
             JSONObject generatedInput = dmpUtil.GenerateDMPInput(baseline, modification);
             System.out.println(JSONObject.toJSONString(generatedInput, SerializerFeature.PrettyFormat));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } 
         }
-
     }
 }
